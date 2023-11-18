45d4af96aa36988c73ce38ac53b6a065fa88e7cd
hongdang <nuyenhongdang@gmail.com>
2023-11-18 17:22:10 +0700

    create function count down date of brith use class or hook

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
-import logo from './logo.svg';
 import './App.css';
 import { Nav } from "./views/Header/Nav";
 import { Home } from './views/Body/HomePage/Home';
 import { ToastContainer } from "react-toastify";
 import 'react-toastify/dist/ReactToastify.css';
-import { ListPokemon } from "./views/Body/Pokemon/ListPokemon"
+import { ListPokemon } from "./views/Body/Pokemon/ListPokemon";
+import { CountDown, CountDownUseHook } from './views/Body/CountDown/CountDown';
 
 function App() {
 
@@ -13,7 +13,9 @@ function App() {
       <header className="App-header">
         <Nav />
         {/* <Home /> */}
-        <ListPokemon />
+        {/* <ListPokemon /> */}
+        <CountDown />
+        <CountDownUseHook />
       </header>
       <ToastContainer
         position="top-right"
---
+++ src/views/Body/CountDown/CountDown.js
@@ -0,0 +1,175 @@
+import React from "react";
+import { useEffect, useState } from "react";
+import "./CountDown.scss";
+
+export class CountDown extends React.Component {
+
+    state = {
+        distance: '',
+        second: '',
+        minute: '',
+        hour: '',
+        day: '',
+        dayBrith: '19',
+        monthBrith: '11'
+    }
+
+    componentDidMount() {
+
+        const second = 1000,
+            minute = second * 60,
+            hour = minute * 60,
+            day = hour * 24;
+
+        //get today and brithday
+        let today = new Date(),
+            dd = String(today.getDate()).padStart(2, "0"),
+            mm = String(today.getMonth() + 1).padStart(2, "0"),
+            yyyy = today.getFullYear(),
+            nextYear = yyyy + 1,
+            { dayBrith, monthBrith } = this.state,
+            birthday = monthBrith + "/" + dayBrith + "/" + yyyy;
+
+        today = mm + "/" + dd + "/" + yyyy;
+
+        if (today > birthday) {
+            birthday = monthBrith + "/" + dayBrith + "/" + nextYear;
+        }
+
+        this.timer = setInterval(() => {
+            const countDown = new Date(birthday).getTime(),
+                now = new Date().getTime(),
+                distance = countDown - now;
+            this.setState({
+                distance: distance,
+                second: Math.floor((distance % minute) / second),
+                minute: Math.floor((distance % hour) / minute),
+                hour: Math.floor((distance % day) / hour),
+                day: Math.floor(distance / day)
+            })
+        }, 0)
+    }
+
+    componentDidUpdate(prevProps, prevState) {
+        if (prevState.distance !== this.state.distance && this.state.distance < 0) {
+            if (this.timer) {
+                clearInterval(this.timer);
+            }
+        }
+    }
+
+    render() {
+
+        let { distance, second, minute, hour, day, dayBrith, monthBrith } = this.state
+
+        return (
+            <>
+                {distance > 0 ?
+                    <>
+                        <h1 id="headline">Countdown to my birthday (CLASS)</h1>
+                        <div id="countdown">
+                            <ul>
+                                <li><span id="days">{day}</span>days</li>
+                                <li><span id="hours">{hour}</span>Hours</li>
+                                <li><span id="minutes">{minute}</span>Minutes</li>
+                                <li><span id="seconds">{second}</span>Seconds</li>
+                            </ul>
+                        </div>
+                    </>
+                    :
+                    <>
+                        <h1 id="headline">Today is {dayBrith + "-" + monthBrith}. It's your brithday</h1>
+                        <div id="content" className="emoji">
+                            <span>🥳</span>
+                            <span>🎉</span>
+                            <span>🎂</span>
+                        </div>
+                    </>
+                }
+            </>
+        )
+    }
+}
+
+
+
+export const CountDownUseHook = () => {
+    const [distance, setDistance] = useState('');
+    const [second, setSecond] = useState('');
+    const [minute, setMinute] = useState('');
+    const [hour, setHour] = useState('');
+    const [day, setDay] = useState('');
+    const [dayBrith, setDayBrith] = useState('');
+    const [monthBrith, setMonthBrith] = useState('');
+
+    useEffect(() => {
+        if (distance < 0) {
+            return;
+        }
+
+        const second = 1000,
+            minute = second * 60,
+            hour = minute * 60,
+            day = hour * 24;
+
+        //get today and brithday
+        let today = new Date(),
+            dd = String(today.getDate()).padStart(2, "0"),
+            mm = String(today.getMonth() + 1).padStart(2, "0"),
+            yyyy = today.getFullYear(),
+            nextYear = yyyy + 1,
+            dayBrith = 19,
+            monthBrith = 11,
+            birthday = monthBrith + "/" + dayBrith + "/" + yyyy;
+        setDayBrith(dayBrith);
+        setMonthBrith(monthBrith);
+        today = mm + "/" + dd + "/" + yyyy;
+
+        if (today > birthday) {
+            birthday = monthBrith + "/" + dayBrith + "/" + nextYear;
+        }
+
+        let timer = setInterval(() => {
+            const countDown = new Date(birthday).getTime(),
+                now = new Date().getTime(),
+                distance = countDown - now;
+            setDistance(distance);
+            setSecond(Math.floor((distance % minute) / second));
+            setMinute(Math.floor((distance % hour) / minute));
+            setHour(Math.floor((distance % day) / hour));
+            setDay(Math.floor(distance / day));
+        }, 0)
+
+        return () => {
+            clearInterval(timer);
+        }
+    }, [distance, dayBrith, monthBrith])
+
+
+    return (
+        <>
+            {distance > 0 ?
+                <>
+                    <h1 id="headline">Countdown to my birthday (HOOK)</h1>
+                    <div id="countdown">
+                        <ul>
+                            <li><span id="days">{day}</span>days</li>
+                            <li><span id="hours">{hour}</span>Hours</li>
+                            <li><span id="minutes">{minute}</span>Minutes</li>
+                            <li><span id="seconds">{second}</span>Seconds</li>
+                        </ul>
+                    </div>
+                </>
+                :
+                <>
+                    <h1 id="headline">Today is {dayBrith + "-" + monthBrith}. It's your brithday</h1>
+                    <div id="content" className="emoji">
+                        <span>🥳</span>
+                        <span>🎉</span>
+                        <span>🎂</span>
+                    </div>
+                </>
+            }
+        </>
+    )
+}
