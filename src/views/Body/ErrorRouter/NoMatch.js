65c53ecd4761a3512e87a7500c5071265c5ce65f
hongdang <nuyenhongdang@gmail.com>
2023-11-21 17:06:52 +0700

    404 page not found

--- src/views/Body/ErrorRouter/NoMatch.js
+++ src/views/Body/ErrorRouter/NoMatch.js
@@ -1,7 +1,15 @@
+import { Link } from "react-router-dom/cjs/react-router-dom";
+
 export const NoMatch = () => {
     return (
         <>
             <h1>404 not found</h1>
+            <button className="btn btn-primary" style={{ 'margin-top': '50px' }}>
+                <Link to="/"
+                    style={{ 'color': "white", 'text-decoration': 'none' }}>
+                    Return home page
+                </Link>
+            </button>
         </>
     )
 }
